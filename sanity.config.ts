b89ff5060d53a1002960c19637f9c1b8aa68c2fa
leolegrand <88097360+leolegrand@users.noreply.github.com>
2023-04-10 18:19:44 +0200

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'lime-mosquito',
+
+  projectId: 'v1vbd0i1',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
